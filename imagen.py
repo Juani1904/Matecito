cbd4feebfe2f7596da9852ef699537c52838f54c
Juan Ignacio <juanicei99@gmail.com>
2022-11-18 16:11:10 -0300

    Last changes before major code restructuration

--- imagen.py
+++ imagen.py
@@ -26,6 +26,7 @@ class Imagen:
         #Aplicamos el preprocesamiento
         self.preProcesamiento()
         #Creamos un manejo de excepcion para solucionar el error ZeroDivision
+        #Si la imagen no toma bien el contorno y por ende el perimetro, se ajusta la frec. de corte de los filtros
         while(True):
             try:
                 #Aplicamos filtro pasa bajo para eliminar el ruido
@@ -41,13 +42,14 @@ class Imagen:
                 self.getCaractVector()
                 break
             except ZeroDivisionError:
+                #Con esto disminuimos la frec de corte del filtro PB y aumentamos la del filtro PA
                 self.sigmaPB-=0.01
                 self.sigmaPA+=0.001
                 continue
         
         
     def preProcesamiento(self):
-        #Aca vamos a aplicar funciones de dilaracion y erosion para mejorar algunos aspectos de la imagen
+        #Aca vamos a aplicar funciones de dilatacion y erosion para mejorar algunos aspectos de la imagen
         #Estos aspectos a mejorar me van a permitir que el algoritmo identifique bien y cierre los perimetros de las figuras
         #De esta manera me aseguro que las figuran tengan perimetros cerrados y no haya error en la toma de puntos de contorno
         #Aplicamos erode y dilate
@@ -84,27 +86,27 @@ class Imagen:
         #Aca se va a implementar el algoritmo de deteccion de contornos
 
         #Buscamos los puntos de contorno
-        self.contornos,_=cv2.findContours(self.imagen,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-        #Buscamos los puntos de contorno que me llevan al casco convexo
+        self.contornos,_=cv2.findContours(self.imagen,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        #Con esta funcion sorted, mejoramos la toma de contorno de manera que se tome bien dicho contorno y se
+        #pueda calcular el perimetro y el area sin problemas
         self.contornos = sorted(self.contornos,key=cv2.contourArea,reverse=True)[:1]
         # Defectos convexos
-        hull = cv2.convexHull(self.contornos[0],returnPoints=False)
-        self.defectosConvex = cv2.convexityDefects(self.contornos[0],hull)
+        self.hull = cv2.convexHull(self.contornos[0],returnPoints=True)
+        #self.defectosConvex = cv2.convexityDefects(self.contornos[0],self.hull)
 
 
     def getCaractVector(self):
         #Creamos el vector de caracteristicas
         self.caractVector=[]
-        #Llenamos el vector de caracteristicas con las distintas caract. geometricas
+        #Calculamos el area y el perimetro de la figura que se armo con la deteccion del contorno
         areafig=cv2.contourArea(self.contornos[0])
         perimetrofig=cv2.arcLength(self.contornos[0],True)
         
         #Calculamos la circularidad
         #Mientras mas cerca de uno la circularidad, mas se parece a un circulo perfecto
         circularidad=(4*(m.pi)*areafig)/(perimetrofig)**2
-        self.caractVector.append(circularidad)
+        #self.caractVector.append(circularidad)
 
-        
         #Calculamos la elasticidad
         #La elasticidad es lo mismo que el aspect ratio (width(ancho)/length(altura))
         #Utilizamos boundingRect
@@ -117,14 +119,22 @@ class Imagen:
         #Lo que vamos a hacer es sumar al vect. de caracteristicas la cantidad de elementos de approxPoly
         #Este numero de elementos corresponde a los "vertices de la figura"
         #Esto es especialmente util para diferencial la arandela de la tuerca
-
         epsilon=0.05*cv2.arcLength(self.contornos[0],True)
         approxPoly=cv2.approxPolyDP(self.contornos[0],epsilon,True)
         self.caractVector.append(len(approxPoly))
 
-        #Obtenemos el 3 momento de hu de la imagen
-        #huMoments=cv2.HuMoments(cv2.moments(self.contornos[0]))
-        #self.caractVector.append(huMoments[2][0])
+        #Calculamos la solidez o "Solidity" de la figura
+        #Es la relacion entre el area de la figura y el area del casco convexo (Convex hull)
+        areaConvex=cv2.contourArea(self.hull)
+        solidez=areafig/areaConvex
+        #self.caractVector.append(solidez)
+
+        #Calculamos momento de Hu
+        momentosHu=cv2.HuMoments(cv2.moments(self.contornos[0])).flatten()
+        self.caractVector.append(momentosHu[0])
+
+
+        
 
         
 
